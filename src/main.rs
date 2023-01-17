4eb4b9b4b4521a9e58e367f070089734072e588d
JuanGarcia0323 <juan.garcia@aridosoftware.com>
2023-01-17 12:38:05 -0300

    working version

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,48 @@
-use csv::WriterBuilder;
-use polars::prelude::{CsvReader, CsvWriter, SerReader, SerWriter};
-// use regex::Regex;
-// use csv::Writer;
-// use polars::prelude::*;
 use regex::Regex;
 use std::{
-    fs::{self, File},
-    // io::{BufWriter, Cursor, Write},
-    io::Write,
+    fs::{self, create_dir_all, File},
+    io::{ErrorKind, Write},
 };
 
 fn main() {
-    for file in fs::read_dir("C:/Users/Juan Emilio/Desktop/archivo_javi/files").unwrap() {
+    let formated_files_path = String::from("./formated-files/");
+    let files_path = String::from("./files-to-convert");
+    create_dir_all(&formated_files_path).unwrap();
+    let re = Regex::new(r"API").unwrap();
+    let finded_files = fs::read_dir(&files_path).unwrap_or_else(|err| {
+        if err.kind() == ErrorKind::NotFound {
+            create_dir_all(&files_path).unwrap();
+            panic!("Insert log files into the files-to-convert folder");
+        }
+        panic!("{}", err)
+    });
+
+    return for file in finded_files {
         let f = file.unwrap();
-        let name = format!("{}.txt", f.file_name().to_str().unwrap());
+        let file_name = f.file_name().to_str().unwrap().replace(".log", ".csv");
+        println!("{}", &file_name);
+        let new_path = format!("{}{}", formated_files_path, file_name);
         let path = f.path();
-        // let name = format!("{}.txt", &path.to_str().unwrap());
-        let re = Regex::new(r"API").unwrap();
-        let mut file = File::create(&name).unwrap();
+        let mut file = File::create(&new_path).unwrap();
         let content = fs::read_to_string(path).unwrap();
         let lines = content.lines();
         let mut first = false;
-        // let mut data: Vec<u8> = vec![];
         for l in lines {
             if re.is_match(l) {
                 let result = if !first {
                     String::from(l)
+                        .replace(r#"""#, "")
+                        .replace(" ", ",")
+                        .replace(",", ", ")
                 } else {
                     format!("\n{}", l)
+                        .replace(r#"""#, "")
+                        .replace(" ", ",")
+                        .replace(",", ", ")
                 };
                 first = true;
                 file.write(result.as_bytes()).unwrap();
             }
         }
-        // let result = Cursor::new(&data);
-        // let mut df = CsvReader::new(result).finish().unwrap();
-        // CsvWriter::new(data).finish(&mut df).unwrap();
-        // break;
-    }
+    };
 }
